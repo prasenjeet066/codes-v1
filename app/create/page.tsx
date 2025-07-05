3b2e1d62e9ed2d8608011919d620e9fb0746388e
prasenjeet066 <prasenjeethowlader006@gmail.com>
2025-07-05 12:33:43 +0600

    Update page.tsx

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 import { createServerClient } from "@/lib/supabase/server"
-import { CreatePostPage } from "@/components/create/create-post-page"
+import CreatePostPage  from "@/components/create/create-post-page"
 
 export default async function CreatePage() {
   const supabase = createServerClient()
